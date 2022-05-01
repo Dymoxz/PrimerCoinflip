931a4731cf69b20fbd412bd4f62549a37eb3c54c
Dymo Waltheer <210532@edu.ozhw.nl>
2022-05-02 01:31:59 +0200

    mnmhj

--- DataCollector.py
+++ DataCollector.py
@@ -1,4 +1,22 @@
-import selenium
 import json
 import time
+import mouse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+
+driver = webdriver.Chrome(chrome_options=options)
+
+driver.get("https://www.primerlearning.org")
+driver.execute_script("window.scrollTo(0, 1080)") 
+
+time.sleep(2)
+
+import pyautogui
+pyautogui.moveTo(1290, 810)
